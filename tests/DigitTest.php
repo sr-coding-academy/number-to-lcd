afbafc5226918284967e9089f5f7020d4141920e
nockschii <Be9zAlksLuws8Jvrws2X>
2019-03-26 14:29:42 +0100

    Tests if digits file is readable.

--- tests/DigitTest.php
+++ tests/DigitTest.php
@@ -2,13 +2,18 @@
 
 namespace NumberToLCDTests;
 
+use NumberToLCD\Digit;
 use PHPUnit\Framework\TestCase;
 
 class DigitTest extends TestCase
 {
     public function testDigitsTextFileExists()
     {
-        $fileLocation = __DIR__ . '\..\resources\digits.txt';
-        $this->assertFileExists($fileLocation);
+        $this->assertFileExists(Digit::DIGIT_TEMPLATE_FILE_PATH);
+    }
+
+    public function testDigitsTextFileIsReadable()
+    {
+        $this->assertFileIsReadable(Digit::DIGIT_TEMPLATE_FILE_PATH);
     }
 }
