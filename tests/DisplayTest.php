20620c42dbfb9541cf52e52be2409ba279096d96
r.quidet <r.quidet@sportradar.com>
2019-05-21 15:00:28 +0200

    Adds empty DisplayTest class.

---
+++ tests/DisplayTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NumberToLCDTests;
+
+use PHPUnit\Framework\TestCase;
+
+class DisplayTest extends TestCase
+{
+    public function testDummyTest()
+    {
+        $this->assertTrue(true);
+    }
+}
